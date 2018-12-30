8d0ddb4cd9d35f637997cd0f8e377cf130e6807f
bjoukovs <borisjoukov@hotmail.com>
2018-12-30 18:16:33 +0100

    Deutsch not Working git add -A!

--- tests/prot3_v9000/deutsch-not-alice.py
+++ tests/prot3_v9000/deutsch-not-alice.py
@@ -19,7 +19,7 @@ def main():
         #
         # let Bob compute this blindly using the following circuit:
         #
-        # To see the circuit, checkout Deutch_not.JPG      
+        # To see the circuit, checkout Deutch_not.JPG or link below  
         #
         # measuring the output of Bob's circuit in the X basis implicitly applies the last H
         # gates needed for the Deutsch-Jozsa algorithm
@@ -30,11 +30,11 @@ def main():
         P = int(N / M)
         L = 3  # Number of steps for protocol2
 
-        D_gates = [ TensorGate([CustomDiagonalGate([0, 32]), 'I']),\
+        D_gates = [ TensorGate([CustomDiagonalGate([0, 32]), 'Z']),\
                         [ TensorGate([CustomDiagonalGate([-16, 16]), 'I']) ],\
                         [ TensorGate(['Z', 'I']) ],\
                         [ TensorGate([CustomDiagonalGate([16, -16]), 'I']) ],\
-                        [ TensorGate([CustomDiagonalGate([0, -32]), 'Z']) ],\
+                        [ TensorGate([CustomDiagonalGate([0, -32]), 'I']) ],\
                         [ EntangleGate('CPHASE') ],\
                         [ TensorGate(['Z', 'I']) ],\
                         [ TensorGate(['I', 'I'])] ]
@@ -45,3 +45,9 @@ def main():
         print('\nRESULT:', result)
 
 main()
+
+
+'''
+Circuit can be found on
+https://algassert.com/quirk#circuit={%22cols%22:[[%22H%22,%22H%22],[%22~ega%22,%22Z%22],[%22H%22,%22H%22],[%22~dqmn%22],[%22H%22,%22H%22],[%22Z%22],[%22H%22,%22H%22],[%22~bel6%22],[%22H%22,%22H%22],[%22~uals%22],[%22H%22,%22H%22],[%22%E2%80%A2%22,%22Z%22],[%22H%22,%22H%22],[%22Z%22]],%22gates%22:[{%22id%22:%22~ega%22,%22name%22:%221%22,%22matrix%22:%22{{%E2%88%9A%C2%BD+%E2%88%9A%C2%BDi,0},{0,%E2%88%9A%C2%BD-%E2%88%9A%C2%BDi}}%22},{%22id%22:%22~5i51%22,%22name%22:%222%22,%22matrix%22:%22{{1,0},{0,%E2%88%9A%C2%BD-%E2%88%9A%C2%BDi}}%22},{%22id%22:%22~3gu0%22,%22name%22:%223%22,%22matrix%22:%22{{1,0},{0,%E2%88%9A%C2%BD+%E2%88%9A%C2%BDi}}%22},{%22id%22:%22~uals%22,%22name%22:%224%22,%22matrix%22:%22{{%E2%88%9A%C2%BD-%E2%88%9A%C2%BDi,0},{0,%E2%88%9A%C2%BD+%E2%88%9A%C2%BDi}}%22},{%22id%22:%22~fvjr%22,%22name%22:%222b%22,%22matrix%22:%22{{0.9238795-0.3826834i,0},{0,0.9238795+0.3826834i}}%22},{%22id%22:%22~dqmn%22,%22name%22:%222B%22,%22matrix%22:%22{{0.9238795+0.3826834i,0},{0,0.9238795-0.3826834i}}%22},{%22id%22:%22~bel6%22,%22name%22:%223B%22,%22matrix%22:%22{{0.9238795-0.3826834i,0},{0,0.9238795+0.3826834i}}%22}]}
+'''
